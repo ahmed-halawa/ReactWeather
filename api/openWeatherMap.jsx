7fcb8ff6adbfdcf7d00987eca53a5640d194ac1c
ahmed halawa <ahmedhalawa333@gmail.com>
2017-08-07 02:34:37 -0700

    updateing modal & err message

--- api/openWeatherMap.jsx
+++ api/openWeatherMap.jsx
@@ -9,12 +9,12 @@ module.exports = {
 
         return axios.get(requestUrl).then(function(res){
             if(res.data.doc && res.data.message){
-                throw new Error(res.data.message);
+                throw new Error('City not found');
             }else{
                 return res.data.main.temp;
             }
         }, function(res){
-            throw new Error(res.data.message);
+            throw new Error('City not found');
         });
 
     }
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 var openWeatherMap = require('openWeatherMap');
+var ErrorModal = require('ErrorModal');
 
 var Weather = React.createClass({
     getInitialState: function(){
@@ -11,7 +12,10 @@ var Weather = React.createClass({
     },
     handleSearch: function(location){
         var self = this;
-        self.setState({isLoading: true});
+        self.setState({
+            isLoading: true,
+            errorMessage: undefined
+        });
         openWeatherMap.getTemp(location).then(function(temp){
             self.setState({
                 location: location,
@@ -19,13 +23,15 @@ var Weather = React.createClass({
                 isLoading: false
             });
         }, function(err){
-            self.setState({isLoading: false});
-            alert('Can not find this city');
+            self.setState({
+                isLoading: false,
+                errorMessage: err.message
+            });
             console.log(err);
         });  
     },
     render : function(){
-        var {isLoading, location, temp} = this.state;
+        var {isLoading, location, temp, errorMessage} = this.state;
 
         function renderMessage(){
             if(isLoading){
@@ -35,11 +41,20 @@ var Weather = React.createClass({
             }
         }
 
+        function renderError(){
+            if(errorMessage && typeof errorMessage === 'string'){
+                return (
+                    <ErrorModal message={errorMessage}/>
+                );
+            }
+        }
+
         return (
             <div>
                 <h1 className="text-center">Get Weather</h1>
                 <WeatherForm onSearch={this.handleSearch}/>
                 {renderMessage()}
+                {renderError()}
             </div>
         );
     }
